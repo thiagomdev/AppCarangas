020a124bb58e5badb29de3d3481fa961a799cfb9
Thiago Monteiro <thiagomonteiro.developer@gmail.com>
2023-02-01 11:06:35 -0300

    First setup project.

---
+++ Carangas/AppDelegate.swift
@@ -0,0 +1,14 @@
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        window = UIWindow(frame: UIScreen.main.coordinateSpace.bounds)
+        window?.makeKeyAndVisible()
+        window?.rootViewController = UINavigationController(rootViewController: HomeViewController())
+        return true
+    }
+}
+
